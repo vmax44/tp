51569bd92c6b46e90b63ac034db57c11be2d6481
alekseevmv <alekseevmv@ubuntu>
2014-04-17 00:20:52 +0400

    developing views for organizations controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,8 +1,13 @@
 class OrganizationsController < ApplicationController
   
   def index
-    @organizations=Organization.paginate(page: params[:page], 
+    if current_user.nil?
+      @organizations=Organization.paginate(page: params[:page], 
                                                per_page: 10)
+    else
+      @organizations=current_user.organizations.paginate(page: params[:page],
+                                               per_page: 10)
+    end
   end
   
   def new
@@ -16,6 +21,20 @@ class OrganizationsController < ApplicationController
       redirect_to(organizations_path)
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @organization=Organization.find(params[:id])
+  end
+  
+  def update
+    @organization=Organization.find(params[:id])
+    if @organization.update_attributes(organization_params)
+      flash[:success]="Organization updated successfully"
+      redirect_to(organizations_path)
+    else
+      render 'edit'
     end
   end
   
